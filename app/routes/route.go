450a5b710242599544f1b6ac053b2616729c82fd
Eza <ezabintangramadhan@gmail.com>
2024-06-02 18:42:01 +0700

    fix:routes

--- app/routes/route.go
+++ app/routes/route.go
@@ -5,6 +5,9 @@ import (
 	userData "e-wallet/features/user/data"
 	userHandler "e-wallet/features/user/handler"
 	userService "e-wallet/features/user/service"
+	walletData "e-wallet/features/wallet/data"
+	"e-wallet/features/wallet/handler"
+	walletService "e-wallet/features/wallet/service"
 	encrypts "e-wallet/utils"
 
 	"github.com/labstack/echo/v4"
@@ -13,17 +16,31 @@ import (
 
 func InitRouter(e *echo.Echo, db *gorm.DB) {
 
+	walletDataService := walletData.New(db)
+	walletService := walletService.New(walletDataService)
+	WalletHandler := handler.New(walletService)
+
 	hashService := encrypts.NewHashService()
-	dataService := userData.New(db)
-	userService := userService.New(dataService, hashService)
-	userHandlerAPI := userHandler.New(userService)
+	userData := userData.New(db, walletDataService)
+	userService := userService.New(userData, hashService)
+	userHandler := userHandler.New(userService)
 
-	e.POST("/login", userHandlerAPI.Login)
+	e.POST("/login", userHandler.Login)
 
-	e.POST("/users/customer", userHandlerAPI.RegisterCustomer)
-	e.POST("/users/merchant", userHandlerAPI.RegisterMerchant)
-	e.GET("/users", userHandlerAPI.GetProfileUser, middlewares.JWTMiddleware())
-	e.DELETE("/users", userHandlerAPI.Delete, middlewares.JWTMiddleware())
-	e.PUT("/users", userHandlerAPI.Update, middlewares.JWTMiddleware())
-	e.POST("/users/changeprofilepicture", userHandlerAPI.UpdateProfilePicture, middlewares.JWTMiddleware())
+	e.POST("/users/customer", userHandler.RegisterCustomer)
+	e.POST("/users/merchant", userHandler.RegisterMerchant)
+	e.GET("/users", userHandler.GetProfileUser, middlewares.JWTMiddleware())
+	e.DELETE("/users", userHandler.Delete, middlewares.JWTMiddleware())
+	e.PUT("/users", userHandler.Update, middlewares.JWTMiddleware())
+	e.POST("/users/changeprofilepicture", userHandler.UpdateProfilePicture, middlewares.JWTMiddleware())
+
+	e.GET("/wallets", WalletHandler.GetWalletById, middlewares.JWTMiddleware())
+
+	// e.POST("/topup")
+
+	// e.GET("/histories")
+
+	// e.POST("/transactions")
+	// e.GET("/transactions")
+	// e.PUT("/transactions/:id")
 }
