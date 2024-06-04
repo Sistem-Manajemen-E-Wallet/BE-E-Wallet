71de64e106d23e1e752395561de0e73414a8afeb
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2024-06-04 18:39:33 +0700

    feat: add new interface contract

--- features/product/entity.go
+++ features/product/entity.go
@@ -15,17 +15,19 @@ type Core struct {
 
 type DataInterface interface {
 	Insert(input Core) error
-	SelectAllProduct() ([]Core, error)
+	SelectAllProduct(offset, limit int) ([]Core, error)
 	SelectProductById(id uint) (*Core, error)
 	SelectProductByUserId(id uint) ([]Core, error)
 	Update(id uint, input Core) error
 	Delete(input uint) error
+	CountProductByUserId(id uint) (int, error)
+	CountProduct() (int, error)
 }
 
 type ServiceInterface interface {
 	Create(input Core) error
 	GetProductById(id uint) (*Core, error)
-	GetAllProduct() ([]Core, error)
+	GetAllProduct(offset, limit int) ([]Core, int, error)
 	GetProductByUserId(id uint) ([]Core, error)
 	Update(id uint, input Core) error
 	Delete(input uint, userID uint) error
