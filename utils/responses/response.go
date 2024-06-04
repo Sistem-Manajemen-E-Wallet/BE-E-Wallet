7d792608e92872386f8acfac8cfbabe5521c324f
Eza <ezabintangramadhan@gmail.com>
2024-06-04 18:02:16 +0700

    fix:pagination_response

--- utils/responses/response.go
+++ utils/responses/response.go
@@ -11,3 +11,17 @@ func WebJSONResponse(msg string, data interface{}) MapResponse {
 		Data:    data,
 	}
 }
+
+type MapResponseMeta struct {
+	Message string      `json:"message"`
+	Meta    interface{} `json:"meta,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func WebJSONResponseMeta(msg string, meta interface{}, data interface{}) MapResponseMeta {
+	return MapResponseMeta{
+		Message: msg,
+		Meta:    meta,
+		Data:    data,
+	}
+}
